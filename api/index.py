598353301b8b421707d3511dfc8032bf0d6f116d
Shamitha <shamithaph108@gmail.com>
2026-02-20 02:08:30 +0530

    Fix handler to return response properly

--- api/index.py
+++ api/index.py
@@ -6,4 +6,5 @@ from app import app
 
 def handler(request):
     """Vercel Python handler function"""
-    return app.full_dispatch_request(request)
+    response = app.full_dispatch_request(request)
+    return response
